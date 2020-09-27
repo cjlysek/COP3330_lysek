35e097062fd7361d146e4288310a3b56c4a7e436
Connor Lysek <cjlysek@gmail.com>
2020-09-27 05:41:02 -0400

    Initial Commit, User input to continuously enter weight/height works as well as ensuring only positive values for height and weight are chosen. Next step from here is to work on calculating BMI.

---
+++ lysek_p2/src/App.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class App
+{
+    public static boolean moreInput()
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Would you like to calculate your BMI? Please enter 'Y' or 'N'.");
+        String userInputChoice = in.nextLine();
+
+        while(!(userInputChoice.equalsIgnoreCase("Y")) && !(userInputChoice.equalsIgnoreCase("N")))
+        {
+            System.out.println("Invalid option, please try again");
+            userInputChoice = in.nextLine();
+        }
+        if(userInputChoice.equalsIgnoreCase("Y"))
+        {
+            return true;
+        }
+        else if(userInputChoice.equalsIgnoreCase("N"))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static double getUserHeight()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your height in inches: ");
+        double height = in.nextDouble();
+        in.nextLine();
+
+        while(height <= 0)
+        {
+            System.out.println("Invalid/Not positive input, please enter a correct height: ");
+            height = in.nextDouble();
+            in.nextLine();
+        }
+        return height;
+    }
+
+    public static double getUserWeight()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your weight in pounds: ");
+        double weight = in.nextDouble();
+        in.nextLine();
+
+        while(weight <= 0)
+        {
+            System.out.println("Invalid/Not positive input, please enter a correct weight: ");
+            weight = in.nextDouble();
+            in.nextLine();
+        }
+        return weight;
+    }
+
+    public static void displayBMIInfo()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            //BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            //bmiData.add(bmi);
+
+            //displayBmiInfo(bmi);
+        }
+
+        //displayBmiStatistics(bmiData);
+    }
+}
