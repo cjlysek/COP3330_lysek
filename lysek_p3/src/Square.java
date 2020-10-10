cd4fbd04b290a0ac408d812e44c0e4d3ef90e742
Connor Lysek <cjlysek@gmail.com>
2020-10-10 18:22:37 -0400

    Initial Commit, First test case works

---
+++ lysek_p3/src/Square.java
@@ -0,0 +1,16 @@
+public class Square extends Shape {
+
+
+    public Square(double i) {
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "square";
+    }
+}
